3f186f227e777d661219fc59daebe8325dd091e0
Itachi <itachi@Itachis-MBP.local>
2020-03-01 12:10:07 +0700

    update to prevent merge conflict

--- src/components/singleView.js
+++ src/components/singleView.js
@@ -11,32 +11,9 @@ export default function SingleView() {
   let infos = works.filter((work) => {
     return work.url === view
   })
-  // let logos = works.tools.map((icon, index) => {
-  //   return <img className="icon" src={icon} alt="web languages" key={index} />
-  // })
-  // console.log(logos)
   let work = infos[0]
-  // let logos = work.tools.map((icon, index) => {
-  //   return <img className="icon" src={icon} alt="web languages" key={index} />
-  // })
   return <React.Fragment>
-    {/* <section className="bg-black">
-      <div className="py-5 text-center">
-        <h2 className="boldish py-md-3 text-light">{work.topic}</h2>
-        <h3 className="text-primary">{work.title}</h3>
-      </div>
-    </section> */}
     <section className="container-fluid no-padding p-sm-5">
-
-      {/* <div className="row py-5">
-        <div className="col-md-4">
-          <h3>Objective :</h3>
-        </div>
-        <div className="col-md-8">
-          <p>{work.objective}</p>
-        </div>
-
-      </div> */}
       <div className="row py-5">
         <div className="col-md-7">
           <img className="img-fluid rounds shadow" src={require(`../img/${work.mainImg}`)} alt={work.title + ' website image'} />
